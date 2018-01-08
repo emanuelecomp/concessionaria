949ce0913c15b1764055bd8ea52a826e520ec8ab
Emanuel Lucas <Emanuel Lucas>
2018-01-08 09:17:08 -0300

    prep for ionic view

---
+++ www/js/controller.js
@@ -0,0 +1,54 @@
+angular.module('starter')
+.controller('ListagemController',function($scope, CarroService){
+    CarroService.obterCarros().then(function(dados){
+        $scope.listaDeCarros = dados;
+    });
+});
+
+angular.module('starter')
+.controller('CarroEscolhidoController',function($stateParams,$scope){
+    $scope.carroEscolhido = angular.fromJson($stateParams.carro);
+    $scope.listaDeAcessorios = [{"nome":"Freio ABS","preco":800},
+                                {"nome":"Ar-condicionado","preco":1000},
+                                {"nome":"MP3 Player","preco":500}   
+                            ];
+    $scope.mudou = function(acessorio, isMarked){
+        if(isMarked){
+            $scope.carroEscolhido.preco = $scope.carroEscolhido.preco + acessorio.preco;
+        }else{
+            $scope.carroEscolhido.preco = $scope.carroEscolhido.preco - acessorio.preco;
+        }
+    }
+});
+
+angular.module('starter')
+.controller('FinalizarPedidoController',function($stateParams,$scope,$ionicPopup,$state,CarroService){
+    $scope.carroFinalizado = angular.fromJson($stateParams.carro);
+    $scope.pedido = {};
+    
+    $scope.finalizarPedido = function(){
+        var pedidoFinalizado = {
+            params: {
+                carro : $scope.carroFinalizado.nome,
+                preco : $scope.carroFinalizado.preco,
+                nome : $scope.pedido.nome,
+                endereco : $scope.pedido.endereco,
+                email : $scope.pedido.email
+            }
+        }
+
+        CarroService.salvarPedido(pedidoFinalizado).then(function(dados){
+            $ionicPopup.alert({
+                title: 'Parabéns',
+                template: 'Compra confirmada'
+            }).then(function(){
+                $state.go('listagem');
+            });
+        },function(erro){
+            $ionicPopup.alert({
+                title: 'Erro',
+                template: 'Campos obrigatórios'
+            });
+        });        
+    }
+});
--- www/js/routes.js
+++ www/js/routes.js
@@ -1,26 +1,21 @@
 angular.module('starter')
-.config(function($stateProvider, $urlRouterProvider){
+.config(function($stateProvider,$urlRouterProvider){
+    $urlRouterProvider.otherwise('listagem');
 
-$urlRouterProvider.otherwise('listagem');
-
-$stateProvider
-
-.state('listagem',{
-	url : '/listagem',
-	templateUrl : 'templates/listagem.html',
-	controller: 'ListagemController'
-})
-
-.state('carroescolhido',{
-	url : '/carroescolhido/:carro',
-	templateUrl: 'templates/carroescolhido.html',
-	controller: 'CarroEscolhidoController'
-})
-
-.state('finalizarpedido',{
-	url : '/finalizarpedido/:carro',
-	templateUrl : 'templates/finalizarpedido.html',
-	controller : 'FinalizarPedidoController'
-})
-
-})
+    $stateProvider
+    .state('listagem',{
+        url: '/listagem',
+        templateUrl: 'templates/listagem.html',
+        controller: 'ListagemController',
+    })
+    .state('carroescolhido',{
+        url: '/carroescolhido/:carro',
+        templateUrl: 'templates/carroescolhido.html',
+        controller: 'CarroEscolhidoController'
+    })
+    .state('finalizarpedido',{
+        url: '/finalizarpedido/:carro',
+        templateUrl: 'templates/finalizarpedido.html',
+        controller: 'FinalizarPedidoController'
+    });
+});
